4bbb763760345094e8403284ba14c9cadcba42dc
Litte-kang <pengkang1212@sina.com>
2015-06-09 10:40:33 +0800

    innotek web server

--- innotek_fw_server.js
+++ innotek_fw_server.js
@@ -2,6 +2,8 @@ var restify = require('restify');
 var fs = require('fs');
 var DevType = require('./node_modules/my_schemas/dev_type');
 var DevInfo = require('./node_modules/my_schemas/dev_info');
+var RemoteCmd = require('./node_modules/remote_cmd/remote_cmd');
+//var command = require('./command');
 
 var httpServer = restify.createServer({
 	
@@ -25,7 +27,7 @@ httpServer.use(function(req, res, next){
 
 httpServer.get('/', function(req, res, next){
 
-	fs.readFile("./public/html/index.html", function(err, data){
+	fs.readFile("./web/index.html", function(err, data){
 	
 		if (err)
 		{
@@ -43,7 +45,7 @@ httpServer.get('/', function(req, res, next){
 
 httpServer.get('/top.html', function(req, res, next){
 
-		fs.readFile("./public/html/top.html", function(err, data){
+		fs.readFile("./web/top.html", function(err, data){
 	
 		if (err)
 		{
@@ -61,7 +63,7 @@ httpServer.get('/top.html', function(req, res, next){
 
 httpServer.get('/device.html', function(req, res, next){
 
-		fs.readFile("./public/html/device.html", function(err, data){
+		fs.readFile("./web/device.html", function(err, data){
 	
 		if (err)
 		{
@@ -79,7 +81,7 @@ httpServer.get('/device.html', function(req, res, next){
 
 httpServer.get('/fw.html', function(req, res, next){
 
-		fs.readFile("./public/html/fw.html", function(err, data){
+		fs.readFile("./web/fw.html", function(err, data){
 	
 		if (err)
 		{
@@ -95,17 +97,135 @@ httpServer.get('/fw.html', function(req, res, next){
 	});
 });
 
-httpServer.get('/device_types', function(req, res, next){
+httpServer.get('/js/my_http.js', function(req, res, next){
+
+		fs.readFile("./public/js/my_http.js", function(err, data){
+	
+		if (err)
+		{
+			console.error(err);			
+			return;
+		}
+		
+		res.setHeader('Content-Type', 'text/js');
+		res.writeHead(200);
+		res.write(data);
+		res.end();
+		next();
+	});
+});
+
+httpServer.get('/db/device_types', function(req, res, next){
 
 	DevType.getDevTypes(res);
 
 });
 
-httpServer.get('/device_infos/:index', function(req, res, next){
+httpServer.get('/db/device_infos/:index', function(req, res, next){
 
 	console.log(req.params);
 	DevInfo.getDevInfos(req.params.index, req.params.slaveDevType, res);
 
+});
+
+httpServer.post('/db/update_fw_cmd', function(req, res, next){
+
+	var cmd;
+	var json = JSON.parse(req.body);
+
+	console.log(req.body);
+
+	if ("null" === json.hostId) //-- update host device --//
+	{
+		for(var i = 0; i < json.slaveId.length; ++i)
+		{
+			var midAddress = "";
+
+			midAddress = json.slaveId[i];
+
+			cmd = RemoteCmd.makeFwUpdateCmd(midAddress, [65535], 6888, json.version, json.fwType);
+
+			console.log(cmd);
+			//--- add command queue ---//
+		}
+	}
+	else	//-- update slave device --//
+	{
+		cmd = RemoteCmd.makeFwUpdateCmd(json.hostId, json.slaveId, 6888, json.version, json.fwType);
+
+		console.log(cmd);
+		//--- add command queue ---//
+	}
+
+	res.writeHead(202);
+	res.write("ok");
+	res.end()
+
+});
+
+httpServer.post("/db/fw_info", function(req, res, next){
+	
+	console.log(req.body);
+
+	res.writeHead(202);
+	res.write("ok");
+	res.end()
+});
+
+httpServer.post("/fw/:fwName", function(req, res, next){
+	
+	console.log(req.params.fwName);
+
+	fs.writeFile(("./public/fw/" + req.params.fwName), req.body, function(err){
+
+		res.writeHead(202);
+		res.end()
+	});
+	
+});
+
+
+//---------------------------------------------------------------------------------------//
+httpServer.get('/test_curve_cmd', function(req, res, next){
+
+		fs.readFile("./public/html/curve_cmd.html", function(err, data){
+	
+		if (err)
+		{
+			console.error(err);			
+			return;
+		}
+		
+		res.setHeader('Content-Type', 'text/html');
+		res.writeHead(200);
+		res.write(data);
+		res.end();
+		next();
+	});	
+});
+
+httpServer.post('/curve_cmd', function(req, res, next){
+
+	console.log(req.body);
+	var json = JSON.parse(req.body);
+	var cmd;
+
+	cmd = ReomteCmd.makeConfigCurveCmd(json.midid, json.slaveid, json.data[0],json.data[1],json.data[2]);
+	console.log(cmd);
+/*
+	        command.addCommand({midAddress:fwInfo.midAddress, address:fwInfo.address}, {midAddress:fwInfo.midAddress, address:fwInfo.address, infoType:4, command:cmdInfo});
+
+	        command.Event.once('save_suc', function(){
+
+	                console.log('command saved success');
+	                res.send(200);
+	                next();
+
+	                command.Event.removeAllListeners();
+	        });
+//*/
+	res.end();
+	next();
 });
 
 httpServer.listen(4040, function(){
